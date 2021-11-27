832480bb71a1c2f702fde4ac736167ec0a6644b0
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2021-11-27 10:53:46 -0600

    set up dummy data and passed the data to useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
+import { useState } from 'react';
 import ExpenseList from './components/ExpenseList';
 import ExpenseForm from './components/ExpenseForm';
 import Alert from './components/Alert';
+import { v4 as uuidv4 } from 'uuid';
+
+const initialExpenses = [
+  { id: uuidv4(), charge: 'rent', amount: 1600 },
+  { id: uuidv4(), charge: 'car payment', amount: 400 },
+  { id: uuidv4(), charge: 'credit card bill', amount: 1200 },
+];
+
+console.log(initialExpenses);
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
   return (
-    <div>
-      hello from app
+    <>
+      <Alert />
       <ExpenseForm />
       <ExpenseList />
-      <Alert />
-    </div>
+    </>
   );
 }
 
