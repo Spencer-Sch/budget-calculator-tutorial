07214c807133ea243f40b2779cb1eca42827a792
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2021-11-29 05:45:14 -0600

    added functionality for adding a new ExpenseItem

--- src/App.js
+++ src/App.js
@@ -14,14 +14,44 @@ const initialExpenses = [
 console.log(initialExpenses);
 
 function App() {
+  // ****************** state values ******************
+  // all expenses, add expense
   const [expenses, setExpenses] = useState(initialExpenses);
+  // single expense
+  const [charge, setCharge] = useState('');
+  // single amount
+  const [amount, setAmount] = useState('');
+  // ****************** functionality ******************
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  };
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== '' && amount > 0) {
+      const singleExpense = { id: uuidv4(), charge, amount };
+      setExpenses([...expenses, singleExpense]);
+      setCharge('');
+      setAmount('');
+    } else {
+      // handle alert called
+    }
+  };
 
   return (
     <>
       <Alert />
       <h1>Budget Calculator</h1>
       <main className="App">
-        <ExpenseForm />
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+        />
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
@@ -29,7 +59,7 @@ function App() {
         <span className="total">
           ${' '}
           {expenses.reduce((acc, curr) => {
-            return (acc += curr.amount);
+            return (acc += parseInt(curr.amount));
           }, 0)}
         </span>
       </h1>
